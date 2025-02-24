68e5591a0f35bcc154a2dd5d785750263db8b16a
ybellaho <yanis.bellahouel76@gmail.com>
2025-02-24 09:01:53 +0100

    style

--- script.js
+++ script.js
@@ -3,19 +3,8 @@ document.addEventListener("DOMContentLoaded", () => {
     const projects = carrousel.getElementsByTagName("li");
     let currentIndex = 0;
 
-    // Création des boutons
-    const prevBtn = document.createElement("button");
-    prevBtn.innerText = "⬅️";
-    prevBtn.classList.add("carrousel-btn", "prev");
-    prevBtn.addEventListener("click", () => moveSlide(-1));
-
-    const nextBtn = document.createElement("button");
-    nextBtn.innerText = "➡️";
-    nextBtn.classList.add("carrousel-btn", "next");
-    nextBtn.addEventListener("click", () => moveSlide(1));
-
-    carrousel.parentNode.insertBefore(prevBtn, carrousel);
-    carrousel.parentNode.appendChild(nextBtn);
+    document.querySelector(".prev").addEventListener("click", () => moveSlide(-1));
+    document.querySelector(".next").addEventListener("click", () => moveSlide(1));
 
     function moveSlide(direction) {
         projects[currentIndex].classList.remove("active");
@@ -23,6 +12,5 @@ document.addEventListener("DOMContentLoaded", () => {
         projects[currentIndex].classList.add("active");
     }
 
-    // Initialisation
     projects[currentIndex].classList.add("active");
 });
